49347ede2f0f3f11acd2beef914bb5c25e81db2e
Slava <migrant405@gmail.com>
2022-07-11 10:56:42 +0400

    Task_001

---
+++ Lesson_001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Enter the first number");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne > numberSecond) 
+{
+    Console.WriteLine("max = " + numberOne + " " + "min = " + numberSecond);
+}
+else
+    Console.WriteLine("max = " + numberSecond + " " + "min = " + numberOne);
