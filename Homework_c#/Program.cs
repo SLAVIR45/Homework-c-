67a3e58774dc5994d69a8becd2ae6deefb345526
Kutukov V.A <Ivanov@bk.ru>
2022-08-08 17:10:41 +0400

    add Lesson_27

--- Homework_c#/Program.cs
+++ Homework_c#/Program.cs
@@ -280,3 +280,24 @@ else
         Console.WriteLine($"число {num_one} в степни {num_two} = {step} ");
     }            
 }
+
+
+void Lesson_27()
+{
+/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+*/
+
+Console.WriteLine("Enter number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int sum = 0;
+while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10; 
+    }
+Console.WriteLine($"сумма цифр числа {temp} = {sum}");
+
+}
+Lesson_27();
