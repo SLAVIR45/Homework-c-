587b912cdc30d1cba13135ff435f111f77cbfc7b
Slava <migrant405@gmail.com>
2022-08-19 16:21:22 +0400

    Lesson_54

---
+++ Homework_c#/Homework_8/Program.cs
@@ -0,0 +1,93 @@
+﻿Console.Clear();
+
+
+
+void Lesson_54()
+{
+    /*
+    Задайте двумерный массив. Напишите программу, 
+    которая упорядочит по убыванию элементы каждой строки двумерного массива.
+    */
+    Console.WriteLine("Eenter the number of rows");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("enter the number of columns");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    int [,] array = new int [rows,columns];
+    random_array(array,0,10);
+    print_array(array);
+    ordering_an_array_rows(array);
+    print_array(array);
+
+
+
+}
+
+
+void ordering_an_array_rows(int [,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    
+    for (int i = 0 ; i < rows; i++)
+    {
+        for (int j = 0 ; j < columns - 1; j++)
+        {
+            for (int k = j + 1; k < columns ; k++)
+            {
+                if (array[i,j] < array[i,k]) 
+                {
+                    int temp = array[i,j];
+                    array[i,j] = array[i,k];
+                    array[i,k] = temp;  
+                }
+                
+            }
+        }
+    }
+}
+
+
+
+
+
+
+void random_array (int[,] array, int start_number = -20 , int finish_number = 20)
+{   
+    finish_number++;
+    Random rand = new Random();
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    
+    for (int i = 0 ; i < rows; i++)
+    {
+        for (int j = 0 ; j < columns; j++)
+        {
+            array[i,j] = rand.Next(start_number, finish_number);
+        }
+    }
+}
+
+
+
+
+void print_array (int [,] array)
+{
+    Console.WriteLine ("Array:");
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    
+    for (int i = 0 ; i < rows; i++)
+    {
+        for (int j = 0 ; j < columns; j++)
+        {
+             Console.Write (array[i,j] + "\t" );
+        }
+         Console.WriteLine();
+    }
+    
+}
+
+
+Lesson_54();
