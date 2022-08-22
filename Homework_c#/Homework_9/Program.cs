7d3684cc0971844e8d9521fa349652051076b43c
Slava <migrant405@gmail.com>
2022-08-22 16:46:31 +0400

    Lesson_64

--- Homework_c#/Homework_9/Program.cs
+++ Homework_c#/Homework_9/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+
+
+void Lesson_64 ()
+{
+     /*
+    Задайте значения M и N. Напишите программу, 
+    которая выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+    */
+
+    Console.WriteLine("Enter the number m");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter the number n");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write ("Числа кратные 3: ");
+    Recurcia (m,n);
+}
+
+void Recurcia (int m , int n )
+    {
+        if ( m > n) return;
+        if (m % 3 == 0)
+        Console.Write (m + ", ");
+        m++;
+        Recurcia(m,n);
+    }
+
+
+
+Lesson_64 ();
